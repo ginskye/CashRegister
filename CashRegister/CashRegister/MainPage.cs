ae7e8b4a24e43481ca05df41b2a878d3f6c846d1
Emily Hall <bibliomaniac@gmail.com>
2022-04-04 21:24:08 -0400

    Add project files.

---
+++ CashRegister/CashRegister/MainPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CashRegister
+{
+    public class MainPage : ContentPage
+        {
+        Entry totalAmount;
+        Button makeChange;
+        Entry totalChange;
+
+        public MainPage()
+        {
+            StackLayout panel = new StackLayout();
+            panel.Orientation = StackOrientation.Vertical;
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a total amount: "
+            });
+
+            panel.Children.Add(totalAmount = new Entry
+            {
+
+            });
+
+            panel.Children.Add(makeChange = new Button
+            {
+                Text = "Calculate Change"
+            });
+
+            panel.Children.Add(totalChange = new Entry
+            {
+
+            });
+            this.Content = panel;
+        }
+    }
+}
